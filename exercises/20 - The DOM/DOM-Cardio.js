f8e3b643c66ef259725c9853b679a364de1dfebd
Erik M. Dodd <emdodd27@gmail.com>
2023-06-15 11:05:39 -0700

    Working on DOM Cardio exercises. More than half way thru

--- exercises/20 - The DOM/DOM-Cardio.js
+++ exercises/20 - The DOM/DOM-Cardio.js
@@ -1,26 +1,65 @@
 // Make a div
+const div = document.createElement('div');
+
 
 // add a class of wrapper to it
+const wrapper = div.classList.add('wrapper');
+
 
 // put it into the body
+document.body.appendChild(div);
 
 // make an unordered list
+const unorderedList = document.createElement('ul');
 
 // add three list items with the words "one, two, three" in them
+const listItem1 = document.createElement('li');
+listItem1.textContent = 'one';
+const listItem2 = document.createElement('li');
+listItem2.textContent = 'two';
+const listItem3 = document.createElement('li');
+listItem3.textContent = 'three';
+
+
 // put that list into the above wrapper
+div.appendChild(listItem1);
+div.appendChild(listItem2);
+div.appendChild(listItem3);
 
 // create an image
+const image = document.createElement('img');
 
 // set the source to an image
+image.src = "https://picsum.photos/500";
+// console.log(image);
+
 // set the width to 250
+image.width = 250;
+
 // add a class of cute
+image.classList.add('cute');
+
 // add an alt of Cute Puppy
+image.alt = "Cute Puppy";
+
 // Append that image to the wrapper
+document.body.appendChild(image);
 
 // with HTML string, make a div, with two paragraphs inside of it
+const htmlString = `
+    <div>
+        <p>Paragraph 1</p>
+        <p>Paragraph 2</p>
+    </div>
+`;
+
 // put this div before the unordered list from above
+unorderedList.insertAdjacentElement('beforebegin', htmlString);
 
 // add a class to the second paragraph called warning
+image.classList.add('warning');
+
+
 // remove the first paragraph
 
 // create a function called generatePlayerCard that takes in three arguments: name, age, and height
