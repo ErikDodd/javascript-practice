a6ed91c319a44afa14ea4f8fb88d224952cfd8b8
Erik M. Dodd <emdodd27@gmail.com>
2023-06-19 13:24:03 -0700

    Almost done with DOM Cardio

--- exercises/20 - The DOM/DOM-Cardio.js
+++ exercises/20 - The DOM/DOM-Cardio.js
@@ -3,8 +3,8 @@ const div = document.createElement('div');
 
 
 // add a class of wrapper to it
-const wrapper = div.classList.add('wrapper');
-
+div.classList.add('wrapper');
+ 
 
 // put it into the body
 document.body.appendChild(div);
@@ -12,19 +12,31 @@ document.body.appendChild(div);
 // make an unordered list
 const unorderedList = document.createElement('ul');
 
+// or this works too
+const ul = `<ul>
+    <li>one</li>
+    <li>two</li>
+    <li>three</li>
+</ul>
+`;
+
 // add three list items with the words "one, two, three" in them
-const listItem1 = document.createElement('li');
-listItem1.textContent = 'one';
-const listItem2 = document.createElement('li');
-listItem2.textContent = 'two';
-const listItem3 = document.createElement('li');
-listItem3.textContent = 'three';
+
+
+// const listItem1 = document.createElement('li');
+// listItem1.textContent = 'one';
+// const listItem2 = document.createElement('li');
+// listItem2.textContent = 'two';
+// const listItem3 = document.createElement('li');
+// listItem3.textContent = 'three';
 
 
 // put that list into the above wrapper
-div.appendChild(listItem1);
-div.appendChild(listItem2);
-div.appendChild(listItem3);
+div.innerHTML = ul;
+
+// div.appendChild(listItem1);
+// div.appendChild(listItem2);
+// div.appendChild(listItem3);
 
 // create an image
 const image = document.createElement('img');
@@ -35,50 +47,60 @@ image.src = "https://picsum.photos/500";
 
 // set the width to 250
 image.width = 250;
+image.height = 250;
 
 // add a class of cute
 image.classList.add('cute');
 
-// add an alt of Cute Puppy
+// add an alt of Cute Puppy 4
 image.alt = "Cute Puppy";
 
 // Append that image to the wrapper
-document.body.appendChild(image);
+div.appendChild(image);
 
 // with HTML string, make a div, with two paragraphs inside of it
 const htmlString = `
-    <div>
+    <div class="myDiv">
         <p>Paragraph 1</p>
         <p>Paragraph 2</p>
     </div>
 `;
+const ulElement = div.querySelector('ul');
 
 // put this div before the unordered list from above
-div.innerHTML = htmlString;
-const myFragment = document.createRange().createContextualFragment(htmlString);
-document.body.appendChild(myFragment);
-// 
+ulElement.insertAdjacentHTML('beforebegin', htmlString);
+
+
+// div.innerHTML = htmlString;
+// const myFragment = document.createRange().createContextualFragment(htmlString);
+// document.body.appendChild(myFragment);
 
 // add a class to the second paragraph called warning
-image.classList.add('warning');
+
+const myDiv = div.querySelector('.myDiv');
+
+myDiv.children[1].classList.add('warning');
 
 
 // remove the first paragraph
-const firstParagraph = myFragment.firstElementChild;
+
+myDiv.firstElementChild.remove();
+// const firstParagraph = myFragment.firstElementChild;
 
 // create a function called generatePlayerCard that takes in three arguments: name, age, and height
 
 function generatePlayerCard(name, age, height) {
-    const newNewDiv = document.createElement('div');
-    newNewDiv.classList.add("playerCard");
-    const h2 = document.createElement('h2');
-    h2.textContent = "${name} - ${age}";
-    const p = document.createElement('p');
-    const ageInDogYears = age * 7;
-    p.textContent = `They are ${height} and ${age} years old. In Dog years this person would be ${ageInDogYears}. That would be a tall dog!`;
+    const html = `
+        <div class="playerCard">
+            <h2>"${name} - ${age}"</h2>
+            <p>Their Height is ${height} and ${age} years old. In Dog years this person would be ${age * 7}. That would be a tall dog!</p>
+            <button class="delete" type="button"> &times; Delete</button>
+        </div> 
+    `;
+    
+    return html;
 
 };
-
 // have that function return html that looks like this:
 // <div class="playerCard">
 //   <h2>NAME — AGE</h2>
@@ -87,15 +109,35 @@ function generatePlayerCard(name, age, height) {
 
 // make a new div with a class of cards
 
-const anotherNewDiv = document.createElement('div');
-anotherNewDiv.classList.add('.cards')
+const cards = document.createElement('div');
+cards.classList.add('cards')
 
 // make 4 player cards using generatePlayerCard
+let cardsHTML = generatePlayerCard('Erik', 34, 69);
+console.log(cardsHTML);
+cardsHTML = cardsHTML + generatePlayerCard('Michele', 38, 63);
+cardsHTML = cardsHTML + generatePlayerCard('Blaise', 1.5, 27);
+cardsHTML = cardsHTML + generatePlayerCard('TuTu', 70, 26);
+
+cards.innerHTML = cardsHTML;
+div.insertAdjacentElement('beforebegin', cards);
+// Could create 4 separate variables for each card
+// could loop over an array of names as another option
 
 // append those cards to the div
 // put the div into the DOM just before the wrapper element
 // Bonus, put a delete Button on each card so when you click it, the whole card is removed
 
 // select all the buttons!
+const buttons = document.querySelectorAll('.delete');
+
 // make out delete function
+function deleteCard(event) {
+    const buttonThatGotClicked = event.currentTarget;
+    // console.log(event.currentTarget);
+
+};
 // loop over them and attach a listener
+buttons.forEach(button => button.addEventListener('click', deleteCard));
+
+console.log(buttons);
