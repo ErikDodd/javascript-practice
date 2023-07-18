2a069d254f7b56d53f4efa74d4ce8409d231ce2c
Erik M. Dodd <emdodd27@gmail.com>
2023-07-18 13:47:54 -0700

    Finished with the functionality of modal. Now just doing CSS stuff

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -1,10 +1,38 @@
 const cardButtons = document.querySelectorAll('.card button');
+const modalInner = document.querySelector('.modal-inner');
+const modalOuter = document.querySelector(".modal-outer");
 
 
 function handleCardButtonClick(event) {
     const button = event.currentTarget;
-    const card = button.closest('.card');
-    console.log(card);
+
+    const card = button.closest('.card'); // closest() is kinda like querySelectorAll but the opposite
+    const imgSrc = card.querySelector('img').src;
+    const description = card.dataset.description;
+    const name = card.querySelector('h2').textContent;
+    modalInner.innerHTML = `
+        <img src="${imgSrc.replace('200', '600', )}" alt="${name}"/>
+        <p>${description}</p>
+    `;
+    // show the modal
+     modalOuter.classList.add('open');
 };
 
-cardButtons.forEach(button => button.addEventListener('click', handleCardButtonClick))
+cardButtons.forEach(button => button.addEventListener('click', handleCardButtonClick));
+
+function closeModal() {
+    modalOuter.classList.remove("open");
+}
+
+modalOuter.addEventListener('click', function(event) {
+    const isOutside = !event.target.closest('.modal-inner');
+    if (isOutside) {
+        closeModal();
+    }
+});
+
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        closeModal();
+    }
+})
