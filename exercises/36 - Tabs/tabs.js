3270f2a12647257f06dee051f69a5dd9909c0544
Erik M. Dodd <emdodd27@gmail.com>
2023-07-20 10:56:32 -0700

    Working on Tabs

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,19 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll("[role='tab']");
+const tabPanels = tabs.querySelectorAll("[role='tabpanel']");
+
+function handleTabClick(event) {
+    // when someone clicks on tab, hide all other tab panels
+    tabPanels.forEach(function(panel) {
+        panel.hidden = true;
+    });
+    // mark all tabs as unselected
+    tabs.forEach(tab => {
+        tab.hidden = true;
+    })
+    // mark the clicked tab as selected
+    // find the associated tab panel and show it 
+
+};
+ 
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
